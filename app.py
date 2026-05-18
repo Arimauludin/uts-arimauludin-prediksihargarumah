c09e1b03837a39d83842be9de02b07874d6d80e6
Arimauludin <arimauludin8888@gmail.com>
2026-05-18 23:42:08 +0700

    Remove alias for streamlit import
    Removed alias for streamlit import.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import streamlit as str_web  # Menggunakan alias agar tidak bentrok
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
